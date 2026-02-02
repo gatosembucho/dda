db7a46aaf609373689af7b7f0156ec535fda706e
gatosembucho <malu.14geraldo@gmail.com>
2026-02-02 11:38:01 -0300

    aula1

---
+++ playground-1.mongodb.js
@@ -0,0 +1,11 @@
+// show databases
+// use ("dda")
+
+// db.pessoas.insertOne({
+//     name:"Malu",
+//     lastname: "Luiza",
+//     salary: 2
+
+// })
+
+use("dda") db.pessoas.insertMany([ { name: "julinha", lastname: "gabriel", salary: 2 }, { name: "jhennie", lastname: "Big", salary: 2 }, ])
