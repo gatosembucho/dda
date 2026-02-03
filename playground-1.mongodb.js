69956a5c584abbbd601d128643f75a54cae8a894
gatosembucho <malu.14geraldo@gmail.com>
2026-02-03 08:51:50 -0300

    aula 2

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,11 +1,31 @@
 // show databases
-// use ("dda")
+use ("dda")
 
-// db.pessoas.insertOne({
-//     name:"Malu",
-//     lastname: "Luiza",
-//     salary: 2
+db.pessoas.insertOne({
+    name:"Malu",
+    lastname: "Luiza",
+    salary: 2
 
-// })
+})
 
-use("dda") db.pessoas.insertMany([ { name: "julinha", lastname: "gabriel", salary: 2 }, { name: "jhennie", lastname: "Big", salary: 2 }, ])
+use("dda") 
+db.pessoas.insertMany([
+     { name: "julinha", lastname: "gabriel", salary: 2 }, 
+     { name: "jhennie", lastname: "Big", salary: 2 }, 
+     { name: "fer", lastname: "fer", salary: 2 }, 
+     { name: "joyce", lastname: "r", salary: 2 }, 
+     { name: "kessy", lastname: "anne", salary: 2 }, 
+     { name: "thay", lastname: "nar", salary: 2 }, 
+     { name: "Ket", lastname: "jones", salary: 2 }, 
+     { name: "Bruna", lastname: "Bonk", salary: 2 }, 
+     { name: "lays", lastname: "coreaninha", salary: 2 }, 
+
+])
+
+use("dda")
+db.pessoas.find({name: "fer"})
+
+
+
+use("dda")
+db.pessoas.find({name: /j/})
