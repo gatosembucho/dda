69ca4066d9caf826e56be11c2be450859fb55ab2
gatosembucho <malu.14geraldo@gmail.com>
2026-02-19 10:55:02 -0300

    aula2finalizada

--- aula2/src/routes/person.ts
+++ aula2/src/routes/person.ts
@@ -25,12 +25,18 @@ router
         const { nome, sobrenome } = req.query
         res.status(200).send({response: nome, sobrenome});
     })
-    .put('/usuarios/:id', (req: Request, res: Response) => {
+    .put('/atualizar/:id', (req: Request, res: Response) => {
         const { id } = req.params;
         const { nome, sobrenome } = req.body;
 
         res.status(200).send(`Pessoa com o id: ${id} foi atualizado para
         ${nome} ${sobrenome}`)
+    })
+
+    .delete('/deletar/:id', (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        res.status(200).send(`Pessoa com o id: ${id} foi deletada `)
     })
 
 export default router;
